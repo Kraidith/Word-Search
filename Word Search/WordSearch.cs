e6fefb544d3daeffac144a351c8a3b4a50a514fc
Logan Winscher <logan.winscher@g.austincc.edu>
2025-02-26 20:21:20 -0600

    Add project files.

---
+++ Word Search/WordSearch.cs
@@ -0,0 +1,122 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Word_Search
+{
+    internal class WordSearch
+    {
+        static void Main(string[] args)
+        {
+            GenerateBoard();
+            ReadCategories();
+            DisplayCategories();
+            string[] board = GenerateBoard();
+            string selectedCategories = DisplayCategories();
+            Console.WriteLine("You chose: " + selectedCategories);
+            // user input is a Console.ReadLine()
+            string[] WordSearchBoard =
+            {
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+             "....................",
+
+            };
+
+            string[] allCategories = {"Anakin", "Luke", "QuiGon", "Revan", "JocastaNu", "BastilleShan", "Jolee", "Ahsoka",
+                "GalenMarek", "ObiWan", "PloKoon", "KitFisto", "BarrissOffee", "CalKestis" };
+            string[] allWords = new string[15];
+            Random rnd = new Random();
+            for (int i = 0; i < allWords.Length; i++)
+            {
+                int randomIndex = rnd.Next(0, allWords.Length);
+                string potentialWord = allWords[randomIndex++];
+                if (allWords.Contains(potentialWord))
+                {
+                    i--;
+                    continue;
+                }
+                allWords[i] = potentialWord;
+            }
+        }
+        static string[] GenerateBoard()
+        {
+            Random rand = new Random();
+            string[] board = new string[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                string row = "";
+                for (int j = 0; j < 20; j++)
+                {
+                    row += (char)rand.Next('A', 'Z' + 1);
+                }
+                board[i] = row;
+            }
+            return board;
+        }
+        static string[] ReadCategories()
+        {
+            int numberOfWords = 15;
+            string filePath = "Words.txt";
+            string[] allCategoriesAndWords = File.ReadAllLines(filePath);
+            string[][] categoriesAndWords = new string[10][];
+            for (int i = 0; i < categoriesAndWords.Length; i++)
+            {
+                categoriesAndWords[i] = new string[numberOfWords];
+                for (int j = 0; j < categoriesAndWords.Length; j++)
+                {
+                    categoriesAndWords[i][j] = allCategoriesAndWords[i * numberOfWords + j];
+                }
+            }
+        }
+        static string DisplayCategories()
+        {
+            string[] categories = ReadCategories();
+
+            if (categories.Length == 0)
+            {
+                Console.WriteLine("No categories found");
+                return "";
+            }
+            foreach (string category in categories)
+            {
+                Console.WriteLine("" + category);
+            }
+
+            string input;
+            while (true)
+            {
+                Console.WriteLine("\n enter a category: ");
+                input = Console.ReadLine();
+
+                if (categories.Contains(input))
+                {
+                    Console.WriteLine("\nValid input");
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input? Try agasin.");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Word Search/Words.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace Word_Search
+{
+    internal class Words
+    {
+        static void Main(string[] args)
+        {
+            GenerateWordsFile();
+        }
+        static void GenerateWordsFile()
+        {
+            string linesFilePath = "Words.txt";
+            StreamWriter writer = new StreamWriter(linesFilePath);
+            string[] linesString =
+            {
+                "Jedi",
+                "Sith",
+                "Ranged",
+                "Melee",
+                "Carnivores",
+                "Herbivores",
+                "Planets",
+                "Races",
+                "Killers",
+            };
+            string[,] categoriesAndWords =
+            {
+             { "Jedi", "Anakin", "Luke", "QuiGon", "Revan", "JocastaNu", "BastilleShan", "Jolee", "Ahsoka", "GalenMarek", "ObiWan", "PloKoon", "KitFisto", "BarrissOffee", "CalKestis"},
+             { "Sith", "Vader", "Nihilus", "Sidious", "Vitiate", "Malak", "Dooku", "Savage", "Malgus", "Talon", "SecondSister", "SeventhSister", "Bane", "Krayt", "Maul" },
+             { "Ranged", "Pistol", "Shotgun", "Rifle", "RocketLauncher", "Plasma", "Energy", "Goo", "Electrical", "FlameThrower", "BloodSiphon", "Decay", "LongNeckRifle", "SniperRifle", "Revolver"},
+             { "Melee", "Sword", "Dagger", "Shield", "Flail", "Mace", "Spear", "Axe", "Halberd", "GreatSword", "DualBlade", "Pike", "Sickle", "Machete", "Club"},
+             { "Carnivores", "TRex", "Carno", "Argi", "Yuti", "Allo", "Spino", "Megalodon", "Wyvern", "Thylacoleo", "Raptor", "Baryonyx", "Carchar", "Troodon", "Managrmr"},
+             { "Herbivores", "Pachy", "Stego", "Iguanadon", "Bronto", "Anky", "Trike", "Doedic", "Therizino", "GigantoRaptor", "Diplo", "Paracer", "Gallimimus", "Chalico", "Mantis"},
+             { "Planets", "Tatooine", "Korribane", "Coruscant", "Kamino", "Kashyyyk", "MalacorFive", "Utapau", "Ilum", "Felucia", "Rishi", "Zeffo", "Vardos", "Batuu", "Eriadu"},
+             { "Races", "Human", "Greenskin", "Norsca", "VampireCounts", "VampireCoast", "High elf", "Wood elf", "Slanesh", "Khorne", "Tzeentch", "Nurgle", "Beastman", "DarkElves", "Dwarfs"},
+             { "Killers", "Wraith", "Doctor", "Huntress", "Clown", "Legion", "Plague", "Trickster", "Mastermind", "DeathSlinger", "Shape", "Blight", "DarkLord", "Unknown", "Knight"},
+
+            };
+            for (int i = 0; i < categoriesAndWords.GetLength(1); i++)
+            {
+                writer.Write(categoriesAndWords[i, 0]);
+                for (int j = 0; j < categoriesAndWords.GetLength(1); j++)
+                {
+                    writer.WriteLine(categoriesAndWords[i, j]);
+                }
+                writer.WriteLine("-");
+            }
+            writer.Close();
+            Console.WriteLine("Words.Txt has been created");
+        }
+    }
+
+}
